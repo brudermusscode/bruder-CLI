84b476f3a012b3df48ff33110e84eb5f811c6a8e
brudermusscode <git@bruder.business>
2025-06-08 22:41:34 +0000

    Updated all

--- logger.sh
+++ logger.sh
@@ -1,14 +1,20 @@
 # File containing logging options which can be included and then
 # used around other sh files. This should keep everything straight forward.
 
+TIME=$(date +"%d. %B %Y - %H:%M")
+
 log_info() {
-  echo "$1 $2"
+  echo "[$1] $2"
 }
 
 log_success() {
-  echo "✅ $1"
+  echo "[✅] $1"
 }
 
 log_error() {
-  echo "💥 $1"
+  echo "[💥] $1"
+}
+
+log_command_output() {
+  sed 's/^/ |»    /' >&2
 }
